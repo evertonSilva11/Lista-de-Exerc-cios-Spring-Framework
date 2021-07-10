a921a0565d8cdcced58ba1ccce6f0aaa208b22e2
evertonSilva11 <87245638+evertonSilva11@users.noreply.github.com>
2021-07-10 17:14:56 -0300

    Add files via upload

---
+++ demo/src/test/java/br/edu/ifrs/restinga/dev1/exercicios/demo/ExerDev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.restinga.dev1.exercicios.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerDev1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
